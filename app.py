7c0e611228b08fc35710ee3762a3bf62e6af8d8e
Nohan Gustavo <81246430+NouanGust@users.noreply.github.com>
2025-05-16 14:18:04 -0300

    aula 16.05

--- app.py
+++ app.py
@@ -1,42 +1,69 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, redirect, request
 import sqlite3
-
 
 app = Flask(__name__)
 
-# Função para conectar no banco de dados
-def pega_connection():
+
+# Função de conexão com banco de dados
+def pega_connetion():
     conn = sqlite3.connect('filmes.db')
     conn.row_factory = sqlite3.Row
     return conn
+    
 
-# HomePage
+# Rota principal -- HomePage
 @app.route('/')
-def Home():
-    conn = pega_connection()
+def HomePage():
+    conn = pega_connetion()
     filmes = conn.execute('SELECT * FROM filmes').fetchall()
     conn.close()
-    return render_template('filmes.html', filmes=filmes)
+    return render_template('index.html', filmes=filmes)
 
 
-
-# Função CREATE
-
+# Rota de adicionar
 @app.route('/adicionar', methods=('GET', 'POST'))
-def adicionar():
+def AdicionarFilme():
     if request.method == 'POST':
         titulo = request.form['titulo']
         genero = request.form['genero']
         nota = request.form['nota']
         
-        conn = pega_connection()
-        conn.execute('INSERT INTO filmes (titulo, genero, nota) VALUES (?, ?, ?)', 
-                     (titulo, genero, nota))
+        conn = pega_connetion()
+        conn.execute('INSERT INTO filmes(titulo, genero, nota) VALUES (?, ?, ?)', 
+                    (titulo, genero, nota))
         conn.commit()
         conn.close()
         return redirect('/')
-    return render_template('adicionar.html')
+    return render_template('adicionarFilmes.html')
+
+# Rota de editar
+@app.route('/editar/<int:id>', methods=('GET', 'POST'))
+def editarFilme(id):
+    conn = pega_connetion()
+    filme = conn.execute('SELECT * FROM filmes WHERE id = ?', (id,)).fetchone()
+    
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        genero = request.form['genero']
+        nota = request.form['nota']
+        
+        conn.execute('UPDATE filmes SET titulo = ?, genero = ?, nota = ? WHERE id = ?',
+                     (titulo, genero, nota, id))
+        conn.commit()
+        conn.close()
+        return redirect('/')
+
+    conn.close()
+    return render_template('editarFilme.html', filme=filme)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)      
+# Rota de Deletar
+@app.route('/deletar/<int:id>')
+def deletarFilme(id):
+    conn = pega_connetion()
+    conn.execute('DELETE FROM filmes WHERE id= ?', (id,))
+    conn.commit()
+    conn.close()
+    return redirect('/')
+
+app.run(debug=True)
--- db_setup.py
+++ db_setup.py
@@ -3,15 +3,14 @@ import sqlite3
 conn = sqlite3.connect('filmes.db')
 cursor = conn.cursor()
 
-cursor.execute('''
-    CREATE TABLE filmes (
+cursor.execute(''' 
+    CREATE TABLE filmes(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         titulo TEXT NOT NULL,
-        genero TEXT NOT NUll,
+        genero TEXT NOT NULL,
         nota INTEGER
-    )
+    )             
 ''')
 
 conn.commit()
 conn.close()
-
